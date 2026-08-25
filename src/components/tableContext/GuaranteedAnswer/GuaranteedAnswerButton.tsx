b5e1ecfc9708489454ebd1d957ef3b50e02f1b8c
aandrey <andrey.akimov@abgroup.tech>
2026-08-25 11:51:10 +0300

    feat: add answer on next slide btn

--- src/components/tableContext/GuaranteedAnswer/GuaranteedAnswerButton.tsx
+++ src/components/tableContext/GuaranteedAnswer/GuaranteedAnswerButton.tsx
@@ -10,12 +10,16 @@ export default function GuaranteedAnswerButton() {
 	const name = useAppSelector((state) => state.room2.name);
 	const persons = useAppSelector((state) => state.room2.persons);
 	const isGameButtonEnabled = useAppSelector((state) => state.room2.isGameButtonEnabled);
-	const canPress = useAppSelector((state) => state.table.canPress);
+	const table = useAppSelector((state) => state.table);
+	const { canPress, content } = table;
+
 	const appDispatch = useAppDispatch();
 	const me = persons.players.find(p => p.name === name);
 	const canAnswer = me && (me.state === PlayerStates.None || me.state === PlayerStates.Lost);
 	// component kills itself so no need to reset
 	const [haveAnswerBeforeFs, setHaveAnswerBeforeFs] = React.useState(false);
+	const [haveAnswerForNextSlide, setHaveAnswerForNextSlide] = React.useState(false);
+
 	React.useEffect(() => {
 		if (canPress && haveAnswerBeforeFs) {
 			console.log('Boom!');
@@ -25,13 +29,32 @@ export default function GuaranteedAnswerButton() {
 		canPress, haveAnswerBeforeFs
 	]);
 
+	React.useEffect(() => {
+		if (canPress && haveAnswerForNextSlide) {
+			console.log('Boom for slide!');
+			appDispatch(pressGameButton());
+		}
+	}, [
+		content?.[0]?.content?.[0]?.value // if content changes
+	]);
+
 	return (
-		<button
-			type='button'
-			className={`playerButton mainAction active ${canAnswer ? '' : ' hidden'} ${canPress ? ' can-press' : ''}`}
-			disabled={!isConnected || !isGameButtonEnabled || !canAnswer}
-			onClick={() => setHaveAnswerBeforeFs(!haveAnswerBeforeFs)}>
-			ОТВЕТИТЬ* {haveAnswerBeforeFs ? '☑️' : ''}
-		</button>
+		<>
+			<button
+				type='button'
+				className={`cheat playerButton mainAction active ${canAnswer ? '' : ' hidden'} ${canPress ? ' can-press' : ''}`}
+				disabled={!isConnected || !isGameButtonEnabled || !canAnswer}
+				onClick={() => setHaveAnswerBeforeFs(!haveAnswerBeforeFs)}>
+				ОТВЕТИТЬ СРАЗУ {haveAnswerBeforeFs ? '☑️' : ''}
+			</button>
+			<button
+				type='button'
+				className={`cheat playerButton mainAction active ${canAnswer ? '' : ' hidden'} ${canPress ? ' can-press' : ''}`}
+				disabled={!isConnected || !isGameButtonEnabled || !canAnswer}
+				onClick={() => setHaveAnswerForNextSlide(!haveAnswerForNextSlide)}>
+				ОТВЕТИТЬ СЛ. СЛАЙД {haveAnswerForNextSlide ? '☑️' : ''}
+			</button>
+		</>
+
 	);
 }
