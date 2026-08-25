30f237f6cef92f05ac519f0e1275f00fb8103b8b
aandrey <andrey.akimov@abgroup.tech>
2026-08-25 12:37:43 +0300

    feat: add copy text btn

--- src/components/gameTable/TextContent/TextContent.tsx
+++ src/components/gameTable/TextContent/TextContent.tsx
@@ -52,11 +52,14 @@ export default function TextContent(props: TextContentProps) {
 	const content = props.animateReading && room.settings.readingSpeed > 0
 		? getAnimatableContent(props.text, room.settings.readingSpeed, animationCycle)
 		: props.text;
-
+	const copyHandler = () => {
+		navigator.clipboard.writeText(props.text);
+	};
 	return (
 		<div className='textHost'>
 			<AutoSizedText className="tableText fadeIn tableTextCenter" maxFontSize={72}>
 				{isAnswer ? <ClickableAnswer text={props.text}>{content}</ClickableAnswer> : content}
+				<button className="copyIcon" onClick={copyHandler}>📋</button>
 			</AutoSizedText>
 		</div>
 	);
